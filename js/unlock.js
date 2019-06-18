82f58a1b211007204d7ac0be36a503ad7939276d
batuhanarican <batuhanarican@hotmail.com>
2019-06-18 20:44:10 +0200

    Unlock actions

---
+++ js/unlock.js
@@ -0,0 +1,72 @@
+var getTest1 = document.querySelector(".locked.test1");
+var getTest2 = document.querySelector(".locked.test2");
+var getTest3 = document.querySelector(".locked.test3");
+var getBalance = document.querySelector(".totaltokens p");
+
+var totalTokens = 300;
+
+getBalance.innerHTML = "<span>Balance:</span><br>" + totalTokens + " tokens";
+
+function unlockTest1() {
+    if (totalTokens >= 50) {
+        this.classList.remove("locked");
+        this.classList.add("unlocked");
+
+        totalTokens -= 50;
+        getBalance.innerHTML = "<span>Balance:</span><br>" + totalTokens + " tokens";
+        
+        setTimeout(
+        function() {
+            getTest1.href = "test.html";
+        }, 100);
+        
+        getTest1.removeEventListener("click", unlockTest1, false);
+    }
+    else {
+        alert("Insufficient funds. Try to play other tests to gain more tokens.");
+    }
+}
+
+function unlockTest2() {
+    if (totalTokens >= 50) {
+        this.classList.remove("locked");
+        this.classList.add("unlocked");
+
+        totalTokens -= 50;
+        getBalance.innerHTML = "<span>Balance:</span><br>" + totalTokens + " tokens";
+        
+        setTimeout(
+        function() {
+            getTest2.href = "#";
+        }, 100);
+        
+        getTest2.removeEventListener("click", unlockTest2, false);
+    }
+    else {
+        alert("Insufficient funds. Try to play other tests to gain more tokens.");
+    }
+}
+
+function unlockTest3() {
+    if (totalTokens >= 200) {
+        this.classList.remove("locked");
+        this.classList.add("unlocked");
+
+        totalTokens -= 200;
+        getBalance.innerHTML = "<span>Balance:</span><br>" + totalTokens + " tokens";
+        
+        setTimeout(
+        function() {
+            getTest3.href = "#";
+        }, 100);
+        
+        getTest3.removeEventListener("click", unlockTest3, false);
+    }
+    else {
+        alert("Insufficient funds. Try to play other tests to gain more tokens.");
+    }
+}
+
+getTest1.addEventListener("click", unlockTest1, false);
+getTest2.addEventListener("click", unlockTest2, false);
+getTest3.addEventListener("click", unlockTest3, false);
